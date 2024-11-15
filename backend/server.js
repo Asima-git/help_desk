158f353cfb7c761f122f6540384937dae9bba9a1
Asima Nandeshwar <nandeshwarasima22@gmail.com>
2024-11-15 08:40:15 -0800

    Cros error resolved

--- backend/server.js
+++ backend/server.js
@@ -11,32 +11,30 @@ const port = process.env.HOST
 const app = express();
 app.use(cookieParser());
 
-app.use(
-  cors({origin: 'https://help-desk-psi-eight.vercel.app',
-      methods: ['GET','POST','DELETE','PUT'],
-      allowedHeaders:[
-          "Content-Type",
-          "Authorization",
-          "Cache-control",
-          'Expires',
-          'Pragma'
-      ],
-      credentials:true
-  })
-)
-app.use(
-cors({origin: 'https://help-desk-psi-eight.vercel.app/',
-    methods: ['GET','POST','DELETE','PUT'],
-    allowedHeaders:[
-        "Content-Type",
-        "Authorization",
-        "Cache-control",
-        'Expires',
-        'Pragma'
-    ],
-    credentials:true
-})
-)
+app.use(cors({
+  origin: 'https://help-desk-psi-eight.vercel.app', 
+  methods: ['GET', 'POST', 'DELETE', 'PUT'],
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "Cache-Control",
+    "Expires",
+    "Pragma"
+  ],
+  credentials: true
+}));
+app.use(cors({
+  origin: 'https://help-desk-psi-eight.vercel.app/', 
+  methods: ['GET', 'POST', 'DELETE', 'PUT'],
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "Cache-Control",
+    "Expires",
+    "Pragma"
+  ],
+  credentials: true
+}));
 // app.use(cookieParser());
 app.use(express.json());
 //Routes
