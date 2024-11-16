f4e1f71f0a7154c97b2ccd9996bab3b132df837a
Asima Nandeshwar <nandeshwarasima22@gmail.com>
2024-11-15 19:12:56 -0800

    cors

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,25 @@ connectToMongo();
 const port = process.env.HOST
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: 'https://help-desk-psi-eight.vercel.app',
+    methods: ['GET', 'POST', 'DELETE', 'PUT'],
+    allowedHeaders: [
+        "Content-Type",
+        "Authorization",
+        "Cache-Control",
+        "Expires",
+        "Pragma"
+    ],
+    credentials: true
+}));
 
+// Custom Middleware to Set Headers
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://help-desk-psi-eight.vercel.app');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+});
 app.use(cookieParser());
 app.use(express.json());
 //Routes
